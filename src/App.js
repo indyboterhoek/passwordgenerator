eb733e712b8874cb60108edcb24053f3350504f8
iboterhoek <iboterhoek@idea11.com.au>
2023-02-15 09:49:51 +1000

    react project for password generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+var RandomWords = require('random-words');
+
+Object.defineProperty(String.prototype, 'capitalise', {
+	value: function () {
+		return this.charAt(0).toUpperCase() + this.slice(1);
+	},
+	enumerable: false
+});
+
+const symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '-', '_', '=', '+', '?'];
+
+function Generator() {
+	let symbol = symbols[Math.floor(Math.random() * symbols.length)];
+	let number = Math.floor(Math.random() * 99);
+	let word1 = RandomWords().capitalise();
+	let word2 = RandomWords().capitalise();
+
+	console.log(symbol, number, word1, word2);
+
+	let passwordTable = [word1, word2, number, symbol];
+
+	let password = '';
+
+	for (let i = 0; i < 4; i++) {
+		let pos = Math.floor(Math.random() * passwordTable.length);
+		console.log(pos);
+		let temp = passwordTable[pos];
+		passwordTable.splice(pos, 1)
+		password += temp;
+	}
+
+	return (
+		<div>
+			<h1>{password}</h1>
+		</div>
+	);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<Generator />
+			</header>
+		</div>
+	);
 }
 
 export default App;
