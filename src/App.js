a632e8b449aa6c876209ca4651307d1888df29d6
iboterhoek <iboterhoek@idea11.com.au>
2023-03-21 15:51:07 +1000

    added notification and styling to clicking for copy to clipboard

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { useState } from 'react'
 var RandomWords = require('random-words');
 var Filter = require('bad-words');
 let filter = new Filter();
@@ -25,33 +26,43 @@ function GetRandomWord() {
 }
 
 export async function copyTextToClipboard(text) {
-	if ('clipboard' in navigator) {
-		return await navigator.clipboard.writeText(text);
-	} else {
-		return document.execCommand('copy', true, text);
-	}
+	navigator.clipboard.writeText(text).then(function () {
+		console.log('Async: Copying to clipboard was successful!');
+	}, function (err) {
+		console.error('Async: Could not copy text: ', err);
+		document.execCommand('copy', true, text);
+	});
 }
 
+let password = '';
+
 function Generator() {
-	let symbol = symbols[Math.floor(Math.random() * symbols.length)];
-	let number = Math.floor(Math.random() * (99 - 10 + 1) + 10);
-	let word1 = GetRandomWord();
-	let word2 = GetRandomWord();
+	const [showAlert, setShowAlert] = useState(false);
 
-	let passwordTable = [word1, word2, number, symbol];
+	if (!showAlert) {
+		let symbol = symbols[Math.floor(Math.random() * symbols.length)];
+		let number = Math.floor(Math.random() * (99 - 10 + 1) + 10);
+		let word1 = GetRandomWord();
+		let word2 = GetRandomWord();
 
-	let password = '';
+		let passwordTable = [word1, word2, number, symbol];
 
-	for (let i = 0; i < 4; i++) {
-		let pos = Math.floor(Math.random() * passwordTable.length);
-		let temp = passwordTable[pos];
-		passwordTable.splice(pos, 1)
-		password += temp;
+		password = '';
+
+		for (let i = 0; i < 4; i++) {
+			let pos = Math.floor(Math.random() * passwordTable.length);
+			let temp = passwordTable[pos];
+			passwordTable.splice(pos, 1)
+			password += temp;
+		}
 	}
 
 	return (
 		<div>
-			<h1 id="password" onClick={() => { copyTextToClipboard(password) }}>{password}</h1>
+			<div>
+				<h1 id="password" onClick={() => { copyTextToClipboard(password); setShowAlert(true) }}>{password}</h1>
+			</div>
+			<div class={"alert alert-success " + (showAlert ? "" : "hidden")} onClick={() => { setShowAlert(false) }}>Password Copied To Clipboard!</div>
 		</div>
 	);
 }
